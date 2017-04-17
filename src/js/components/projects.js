27f86f2f596f09c565564d48a8e43967fbf0027c
campbell <campbellrehu@me.com>
2017-04-17 12:17:55 +1200

    Added new components

---
+++ src/js/components/projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Projects extends React.Component {
+    render() {
+        return (
+            <h1>Projects Page</h1>
+        );
+    }
+}
