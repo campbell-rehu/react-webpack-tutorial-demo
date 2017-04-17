e8e15f91161c9a9b9ca0da87ba69902184516bbc
campbell <campbellrehu@me.com>
2017-04-17 16:09:57 +1200

    Updated App.js

--- src/js/App.js
+++ src/js/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header';
-import Home from './components/home';
-import About from './components/about';
-import Contact from './components/contact';
+
 import Routes from './components/routes';
 
 class App extends React.Component {
